b26a80d3ad8b4e0887bb720cd049a9c0571d15fb
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2021-11-20 23:18:19 +0100

    add updated tests

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ def client():
     APP.config['TESTING'] = True
     client = APP.test_client()
     yield client
-def test_valid_transaction(client):
+def tests_valid_transaction(client):
     card = {
         "status": True,
         "number":123456,
@@ -22,7 +22,7 @@ def test_valid_transaction(client):
     rv = client.post("/api/transaction",json=card)
     assert True == rv.get_json().get("approved")
     assert 500 == rv.get_json().get("newLimit")
-def test_above_limit(client):
+def tests_above_limit(client):
     card = {
     "status": True,
     "number":123456,
@@ -34,7 +34,7 @@ def test_above_limit(client):
     rv = client.post("/api/transaction",json=card)
     assert False == rv.get_json().get("approved")
     assert "Transaction above the limit" in rv.get_json().get("reason")
-def test_blocked_card(client):
+def tests_blocked_card(client):
     card = {
         "status": False,
         "number":123456,
